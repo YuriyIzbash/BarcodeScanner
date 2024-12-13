ccc8e66a56015dbe6aa4190c15151ebc62370228
YuriyIzbash <izbash91@gmail.com>
2024-12-13 15:47:23 +0100

    Initial Commit

---
+++ BarcodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by YURIY IZBASH on 13. 12. 24.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BarCodeScanner
+//
+//  Created by YURIY IZBASH on 13. 12. 24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
